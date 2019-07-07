31c3df015a027c1b2d8ad003d5a4878e64ef7e19
niejian9001@163,com <niejian9001@163.com>
2019-07-07 11:10:13 +0800

    验证码

--- eshop-admin/src/main/java/cn/com/eshop/admin/config/kaptcha/KaptchaConfig.java
+++ eshop-admin/src/main/java/cn/com/eshop/admin/config/kaptcha/KaptchaConfig.java
@@ -1,11 +1,41 @@
-package cn.com.eshop.admin.config.kaptcha;/**
- * Created by niejian on 2019/7/7.
- */
+package cn.com.eshop.admin.config.kaptcha;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 /**
+ * 验证码相关配置
  * @author niejian
  * @date 2019/7/7
  */
-
+@Configuration
 public class KaptchaConfig {
+    @Bean
+    public DefaultKaptcha producer() {
+        Properties properties = new Properties();
+        properties.put("kaptcha.border", "yes");
+        properties.setProperty("kaptcha.border.color", "105,179,90");
+
+//        properties.put("kaptcha.textproducer.font.color", "black");
+        properties.put("kaptcha.textproducer.char.space", "10");
+        properties.put("kaptcha.textproducer.char.length","5");
+        properties.put("kaptcha.image.height","46");
+        properties.put("kaptcha.image.width","180");
+        properties.put("kaptcha.textproducer.font.size","25");
+        properties.setProperty("kaptcha.noise.color", "black");
+        properties.setProperty("kaptcha.background.clear.from", "102,153,153");
+        properties.setProperty("kaptcha.background.clear.to", "183,234,112");
+
+        //kaptcha.obscurificator.impl生成验证码，我理解的就是样式，有三种，水纹，鱼眼，阴影com.google.code.kaptcha.impl.ShadowGimpy，我用的就是这个阴影，感觉这个显示效果最好
+        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.ShadowGimpy");
+        Config config = new Config(properties);
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+
 }
--- eshop-admin/src/main/java/cn/com/eshop/admin/controller/KaptchaController.java
+++ eshop-admin/src/main/java/cn/com/eshop/admin/controller/KaptchaController.java
@@ -1,11 +1,89 @@
-package cn.com.eshop.admin.controller;/**
- * Created by niejian on 2019/7/7.
- */
+package cn.com.eshop.admin.controller;
+
+import cn.com.eshop.common.vo.CommonInstance;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author niejian
  * @date 2019/7/7
  */
 
+@Controller
+@RequestMapping(value = "/captcha")
 public class KaptchaController {
+
+    @Autowired
+    private DefaultKaptcha producer;
+
+    @ResponseBody
+    @GetMapping(value = "/getCaptcha")
+    public void captcha(HttpServletRequest request,
+                                       HttpServletResponse response) throws ServletException, IOException {
+
+        byte[] captchaChallengeAsJpeg = null;
+
+        // 生成文字验证码
+        String text = producer.createText();
+        // 将验证码存放到session中，登录的时候做比较
+        HttpSession session = request.getSession();
+        session.setAttribute(CommonInstance.LOGIN_VALIFY_CODE, text);
+
+        // 生成图片验证码
+        ByteArrayOutputStream outputStream = null;
+        BufferedImage image = producer.createImage(text);
+
+        outputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", outputStream);
+
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+
+        // 定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
+        captchaChallengeAsJpeg = outputStream.toByteArray();
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+//        response.setDateHeader("Expires", 0);
+        response.setContentType("image/png");
+        ServletOutputStream responseOutputStream = response.getOutputStream();
+        try {
+            responseOutputStream.write(captchaChallengeAsJpeg);
+            responseOutputStream.flush();
+            responseOutputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (null != responseOutputStream) {
+
+                responseOutputStream.close();
+
+            }
+
+            if (null != outputStream) {
+                outputStream.close();
+            }
+        }
+
+
+    }
+
 }
